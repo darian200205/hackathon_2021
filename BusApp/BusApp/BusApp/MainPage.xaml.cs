bcd7214108bda6ab0e1893866c4ec55cd7aa7aa6
darian200205 <darianserban@yahoo.com>
2021-11-20 19:23:48 +0200

    single point tracking

--- BusApp/BusApp/BusApp/MainPage.xaml.cs
+++ BusApp/BusApp/BusApp/MainPage.xaml.cs
@@ -5,14 +5,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace BusApp
 {
     public partial class MainPage : ContentPage
     {
+        private readonly TrackCoordinates[] route1Coordinates;
+        // bus' coordinates array
+
+        private Pin pinRoute1 = new Pin()
+        {
+            Label = "Bus x" //random bus pin
+        };
+
         public MainPage()
         {
             InitializeComponent();
+
+            route1Coordinates = getCoordinates(); // gets coordinates
+            myMap.Pins.Add(pinRoute1);
+
+            var index = 0; // coordinates index
+            Xamarin.Forms.Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+            pinRoute1.Position = new Xamarin.Forms.Maps.Position(
+                    route1Coordinates[index].Position.LatitudeDegrees, route1Coordinates[index].Position.LongitudeDegrees);
+                    );
+            });
+
         }
     }
 }
--- BusApp/BusApp/BusApp/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ BusApp/BusApp/BusApp/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace BusApp {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Maps.Map myMap;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            myMap = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Maps.Map>(this, "myMap");
         }
     }
 }
